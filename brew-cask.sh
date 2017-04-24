a783cd07943dce1e240d349198d6cf3cf8025b41
Andreas Sahle <andreas.sahle@wunderkraut.com>
2017-04-24 23:30:30 +0200

    package and app installers update

--- brew-cask.sh
+++ brew-cask.sh
@@ -10,47 +10,52 @@
 brew install caskroom/cask/brew-cask
 brew tap caskroom/versions
 
-# daily
-brew cask install moom
-brew cask install dropbox
-# brew cask install gyazo
 brew cask install 1password
-brew cask install harvest
-brew cask install hipchat
-brew cask install flux
+brew cask install adobe-creative-cloud
 brew cask install alfred
-brew cask install path-finder
-
-
-# dev
-brew cask install phpstorm
-brew cask install iterm2
 brew cask install atom
-brew cask install sublime-text
-brew cask install imagealpha
-brew cask install imageoptim
-brew cask install kaleidoscope
-brew cask install tower
-
-# fun
-brew cask install limechat
-
-
-# browsers
+brew cask install balsamiq-mockups
+brew cask install bartender
+brew cask install caffeine
+brew cask install dash
+brew cask install disk-inventory-x
+brew cask install docker
+brew cask install dropbox
+brew cask install duet
+brew cask install firefox
+brew cask install flickr-uploadr
+brew cask install flux
+brew cask install genymotion
 brew cask install google-chrome
 brew cask install google-chrome-canary
-brew cask install firefox
-# brew cask install webkit-nightly
-# brew cask install chromium
-# brew cask install torbrowser
-
-# less often
-brew cask install nvalt
-brew cask install marked
-brew cask install disk-inventory-x
-brew cask install screenflow
-brew cask install vlc
-brew cask install gpgtools
+brew cask install google-drive
+brew cask install google-photos-backup
+brew cask install gotomeeting
+brew cask install handbrake
+brew cask install imagealpha
+brew cask install imageoptim
+brew cask install iterm2
+brew cask install kaleidoscope
+brew cask install libreoffice
+brew cask install libreoffice-language-pack
 brew cask install licecap
-brew cask install utorrent
+brew cask install limechat
+brew cask install nvalt
+brew cask install path-finder
+brew cask install phpstorm
+brew cask install real-vnc
+brew cask install screenflow
+brew cask install screenhero
+brew cask install sequel-pro
+brew cask install sketch
+brew cask install sublime-text
+brew cask install tower
+brew cask install vagrant
+brew cask install vagrant-manager
+brew cask install virtualbox
+brew cask install vlc
+brew cask install vyprvpn
+brew cask install wordpresscom
+brew cask install zeplin
+brew cask install zoomus
 
--- brew.sh
+++ brew.sh
@@ -8,7 +8,6 @@ brew update
 # Upgrade any already-installed formulae
 brew upgrade
 
-
 # GNU core utilities (those that come with OS X are outdated)
 brew install coreutils
 brew install moreutils
@@ -29,56 +28,60 @@ brew install homebrew/dupes/grep
 brew install homebrew/dupes/openssh
 brew install homebrew/dupes/screen
 
-
 # run this script when this file changes guy.
 brew install entr
 
 # github util. imho better than hub
 brew install gh
 
-
 # mtr - ping & traceroute. best.
 brew install mtr
 
     # allow mtr to run without sudo
-    mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/paulirish/.homebrew/Cellar/mtr/0.86`
+    mtrlocation=$(brew info mtr | grep Cellar | sed -e 's/ (.*//') #  e.g. `/Users/andi/.homebrew/Cellar/mtr/0.86`
     sudo chmod 4755 $mtrlocation/sbin/mtr
     sudo chown root $mtrlocation/sbin/mtr
 
 
 # Install other useful binaries
-# brew install the_silver_searcher
-# brew install fzf
-
 brew install git
+brew install ack
+brew install android-platform-tools
+brew install autoconf
+brew install automake
+brew install awscli
+brew install behat
+brew install boost
+brew install composer
+brew install ffmpeg --with-libvpx
+brew install flow
+brew install freetype
+brew install htop
 brew install imagemagick --with-webp
-brew install node # This installs `npm` too using the recommended installation method
+brew install mcrypt
+brew install openssl
+brew install php-code-sniffer drupal-code-sniffer
+brew install phpunit
 brew install pv
+brew install python
 brew install rename
+brew install rsync
+brew install terminal-notifier
 brew install tree
 brew install zopfli
-brew install ffmpeg --with-libvpx
-
-brew install terminal-notifier
-
-brew install android-platform-tools
+brew install yarn
+brew install youtube-dl
 
 # install local servers and development stuff
+brew install node # This installs `npm` too using the recommended installation method
 brew install mysql
 brew install mongodb
 brew install sqlite
 brew install elasticsearch
+brew install kibana
 brew install php70 php70-mcrypt php70-mongodb php70-opcache php70-xdebug
 brew install nginx
-
-brew install drush
-brew install php-code-sniffer drupal-code-sniffer
-brew install phpunit
-brew install composer
-
-brew install behat
-
-brew install python
+brew install redis
 
 # Remove outdated versions from the cellar
 brew cleanup
