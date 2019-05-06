655d0a5e1d53f0d49022bd8226db81d916ca10bc
Andreas Sahle <andreas.sahle@gmail.com>
2019-05-06 11:12:39 +0200

    feat: modernize npm global packages

--- npm.sh
+++ npm.sh
@@ -1,26 +1,37 @@
+
+npm i -g @aws-amplify/cli
+npm i -g @sanity/cli
+npm i -g @storybook/cli
+npm i -g apollo-codegen
+npm i -g artillery
+npm i -g awsmobile-cli
+npm i -g babel-cli
+npm i -g browser-sync
+npm i -g create-next-app
+npm i -g create-react-app
+npm i -g ember-cli
+npm i -g forever
+npm i -g gatsby-cli
 # Type `git open` to open the GitHub page or website for a repository.
 npm i -g git-open
-
+npm i -g gitbook-cli
+npm i -g graphql-cli
+npm i -g gulp
+npm i -g hygen
+npm i -g ios-sim
+npm i -g jscodeshift
+npm i -g json-server
+npm i -g live-server
+npm i -g nativefier
+npm i -g nodemon
+npm i -g now
+npm i -g npm-check
+npm i -g preact-cli
+npm i -g prisma
+npm i -g reactql
+npm i -g source-map-explorer
 # trash as the safe `rm` alternative
 npm i -g trash-cli
-
-# development
-npm i -g grunt-cli
-npm i -g gulp
-npm i -g forever
-npm i -g live-server
-npm i -g nodemon
-npm i -g webpack
-npm i -g eslint
-npm i -g bower
-npm i -g browser-sync
-npm i -g babel-cli
-npm i -g phantomjs
-npm i -g casperjs
-
-# scaffolding
-npm i -g yo
-npm i -g generator-webapp
-npm i -g ember-cli
-npm i -g hexo-cli
-npm i -g create-react-app
+npm i -g typedoc
+npm i -g typescript
+npm i -g typesync
