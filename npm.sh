cfcb8c1271f5cab0fd3c92ac5d65fac4609c8460
Andreas Sahle <andreas.sahle@gmail.com>
2019-05-06 14:20:31 +0200

    feat: update npm global packages installer

--- npm.sh
+++ npm.sh
@@ -12,8 +12,8 @@ npm i -g create-react-app
 npm i -g ember-cli
 npm i -g forever
 npm i -g gatsby-cli
-# Type `git open` to open the GitHub page or website for a repository.
 npm i -g git-open
+npm i -g git-recent
 npm i -g gitbook-cli
 npm i -g graphql-cli
 npm i -g gulp
@@ -30,7 +30,6 @@ npm i -g preact-cli
 npm i -g prisma
 npm i -g reactql
 npm i -g source-map-explorer
-# trash as the safe `rm` alternative
 npm i -g trash-cli
 npm i -g typedoc
 npm i -g typescript
