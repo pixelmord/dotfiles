c334033a71bbd4000c86dddf8b3011269dbb2f75
Andreas Adam <andreas.adam@composableweb.com>
2021-07-11 12:58:23 +0200

    feat: update npm global libs

--- npm.sh
+++ npm.sh
@@ -1,25 +1,29 @@
+# TODO: reduce to minimum set of usefull tools, the rest can be called with npx on demand. Less maintainance!
 
-# npm i -g @aws-amplify/cli
-# npm i -g @sanity/cli
-# npm i -g @storybook/cli
-# npm i -g apollo-codegen
-# npm i -g artillery
 # npm i -g autocannon
-# npm i -g awsmobile-cli
-# npm i -g babel-cli
+npm i -g @aws-amplify/cli
+npm i -g @sanity/cli
+npm i -g @storybook/cli
+npm i -g apollo-codegen
+npm i -g artillery
+npm i -g aws-cdk
+npm i -g awsmobile-cli
+npm i -g blitz
 npm i -g browser-sync
+npm i -g caprover
 npm i -g create-next-app
 npm i -g create-react-app
 npm i -g degit
 npm i -g diff-so-fancy
-# npm i -g ember-cli
-# npm i -g forever
-# npm i -g gatsby-cli
+npm i -g dockly
+npm i -g fast-cli
+npm i -g firebase-tools
+npm i -g forever
 npm i -g git-open
 npm i -g git-recent
-# npm i -g gitbook-cli
-# npm i -g graphql-cli
-# npm i -g gulp
+npm i -g gitbook-cli
+npm i -g graphql-cli
+npm i -g husky
 npm i -g hygen
 npm i -g ios-sim
 npm i -g jscodeshift
@@ -27,17 +31,16 @@ npm i -g json-server
 npm i -g live-server
 npm i -g nativefier
 npm i -g nodemon
+npm i -g npkill
+npm i -g npm
 npm i -g npm-check
-# npm i -g preact-cli
 npm i -g prettier
-# npm i -g prisma
-# npm i -g reactql
+npm i -g prisma
+npm i -g reactql
 npm i -g source-map-explorer
 npm i -g sync-dotenv
 npm i -g trash-cli
 npm i -g typedoc
 npm i -g typescript
 npm i -g typesync
-# npm i -g xo
-npm i -g dockly
 npm i -g vercel
